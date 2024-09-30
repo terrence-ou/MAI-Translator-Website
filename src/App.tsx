9fb527042c6944f6787cc23d3766dd9912121e1b
terrence-ou <outerrencedl@gmail.com>
2024-09-29 21:56:17 -0400

    theme updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { createHashRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
 import Root from "./routes/root";
 
 const router = createHashRouter([{ path: "/", element: <Root /> }]);
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,47 @@
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { Sun, Moon } from "lucide-react";
 import appIcon from "@/assets/icon.png";
 
 const Header = () => {
+  const [currTheme, setCurrTheme] = useState<"dark" | "light">(
+    localStorage.getItem("theme")! as "dark" | "light",
+  );
+  const handleSwitchCurrTheme = () => {
+    if (currTheme === "dark") {
+      document.documentElement.classList.remove("dark");
+      localStorage.theme = "light";
+      setCurrTheme("light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.theme = "dark";
+      setCurrTheme("dark");
+    }
+  };
+
   return (
-    <header className="frame border-b h-16 px-root-x bg-slate-100 shadow-md">
+    <header className="frame border-b h-16 px-root-x bg-background shadow-md">
       <div className="frame flex items-center justify-between h-full border-x px-8">
         <div className="flex items-center gap-8">
           <div className="flex items-center gap-3">
             <img src={appIcon} className="w-10" />
             <h2 className="text-xl font-bold drop-shadow-xl">MAI Translator</h2>
           </div>
+          <div>Get Started</div>
           <div>Releases</div>
         </div>
-        <div className="flex items-center gap-8">
-          <div>Theme</div>
+        <div className="flex items-center gap-4">
+          <Button
+            className="w-7 h-7 p-0"
+            variant="outline"
+            onClick={handleSwitchCurrTheme}
+          >
+            {currTheme === "light" ? (
+              <Sun className="w-5" strokeWidth={"1.5px"} />
+            ) : (
+              <Moon className="w-5" strokeWidth={"1.5px"} />
+            )}
+          </Button>
           <Button>Download</Button>
         </div>
       </div>
